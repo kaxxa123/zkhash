39fcea51e1d727e23be6634f82389f65becc3b0e
kaxxa123 <axzammit@hotmail.com>
2021-11-27 14:35:18 +0100

    Updates for demo to work with latest zokrates

--- SmartContract/test/test01.js
+++ SmartContract/test/test01.js
@@ -7,7 +7,13 @@ contract('Testing Verifier contract', function(accounts) {
         let ver = await Verifier.deployed();
         let proof = JSON.parse(fs.readFileSync("./test/proof.json"));
 
-        let res = await ver.verifyTx(proof.proof.a, proof.proof.b, proof.proof.c, proof.inputs);
+        //
+        // ZoKrates changed a bit the verifier function signature...
+        // Old zoKrates Verifier
+        // let res = await ver.verifyTx(proof.proof.a, proof.proof.b, proof.proof.c, proof.inputs);
+
+        // Latest zoKrates Verifier (27th Nov 2021)
+        let res = await ver.verifyTx(proof.proof, proof.inputs);
         assert(res,"Verification FAILED!");
         console.log(`Verify Result: ${res}`);
 
